9bbaa2de2f2deb438b9a53e8dab836cf4e9f841c
gabriel9966 <87774582+gabriel9966@users.noreply.github.com>
2023-07-18 15:08:48 -0300

    1

---
+++ console1/ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("oi");
+            int idade = 20;
+            int maior = 18;
+//bloco region serve para organizar código
+            #region primeiro bloco
+            if (idade >= maior) {
+                Console.WriteLine("Pode entrar");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar");
+            }
+            #endregion
+            /*
+             comentario
+             */
+            Console.ReadKey();// ele para o programa e espera o usuario aperta qualquer tecla para continuar
+            Console.WriteLine("fim");
+            int o = 10;
+        }
+    }
+}
