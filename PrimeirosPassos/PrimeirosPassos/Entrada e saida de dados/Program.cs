66c0484981d48810fef90ea393c0adba22ffa695
gabriel9966 <87774582+gabriel9966@users.noreply.github.com>
2023-07-19 15:47:35 -0300

    Entrada e saída de dados

--- PrimeirosPassos/PrimeirosPassos/Entrada e saida de dados/Program.cs
+++ PrimeirosPassos/PrimeirosPassos/Entrada e saida de dados/Program.cs
@@ -10,11 +10,32 @@ namespace Entrada_e_saida_de_dados
     {
         static void Main(string[] args)
         {
+            #region Entrada e saida(dados)
             //saída
-            Console.Write(",");
-            Console.WriteLine("oi");
-            Console.Write(",");
-            Console.ReadKey();
+            //Console.Write(",");
+            //Console.WriteLine("oi");
+            //Console.Write(",\n");
+
+            //Console.ReadKey();
+            //Entrada
+            //int idadeee = Console.Read(); captura o código de um digito
+            //Console.WriteLine(idadeee);
+
+            //scanner de String
+            //String nome = Console.ReadLine();//readline tem retorno do tipo String
+            // Console.WriteLine(nome);
+
+            Console.WriteLine("Digite um numero:");
+            int num = int.Parse(Console.ReadLine());//faz a leitura de numeros inteiros
+            Console.WriteLine("Digete outro numero:");
+            int num2 = int.Parse(Console.ReadLine());
+            int total = num + num2;
+            Console.WriteLine("A soma dos numeros deu:"+total);
+            #endregion
+
+
+
+
 
         }
     }
