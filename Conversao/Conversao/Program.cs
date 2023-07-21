49ad158fa89dcf9cffdd4a2d7af0fce7cc7f6c75
gabriel9966 <87774582+gabriel9966@users.noreply.github.com>
2023-07-20 23:24:28 -0300

    Conversao

--- Conversao/Conversao/Program.cs
+++ Conversao/Conversao/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -41,10 +42,64 @@ namespace Conversao
             int inteiro = 300000000;
             byte intei = (byte)inteiro;
             Console.WriteLine(intei);
+            //implicita
+            float n = 0f;//32bits
+            double o = n;//64bits
+            int i = 29;
+            double ii = i;
+
+            float l = 9f;//32bits
+            int ll = (int)l;
 
 
 
+            #endregion
 
+            #region parse
+            //trasnforma uma cadeia de caracteres (String) em número, padrão correspondente
+            //parse faz a conversão de string
+            String txtNum = "1985";
+            int numeral = int.Parse(txtNum);//converou para int
+                                            // int numeral2 = (int)txtNum;
+                                            //Console.WriteLine(numeral);
+
+
+            String txtNum2 = "1234,99";
+            double dbtxt = double.Parse(txtNum2);
+            double dbb =199.99;
+            Console.WriteLine(dbtxt);
+
+
+            #endregion
+
+
+            #region convert
+            //converte quarquer tipo de dado para qualquer tipo de dado
+
+            String texto = Convert.ToString(99.990);
+            Console.WriteLine(texto);
+
+            int aaaa = Convert.ToInt32(67.99);
+            Console.WriteLine(aaaa);
+
+            double ann = Convert.ToDouble(true);
+            Console.WriteLine(ann);
+
+            Object ob = 99.0;
+            Console.WriteLine(ob) ;
+
+            bool myBoolValue = true;
+            int myIntValue = Convert.ToInt32(false);
+
+
+            Console.WriteLine(myIntValue) ;
+
+            double dbbb = Convert.ToDouble("9");//so da para converter string numerica, nao letras e palavras, nao aceita char
+            Console.WriteLine(dbbb) ;
+
+            int innnnt = Convert.ToInt32('a');
+            Console.WriteLine(innnnt) ;
+            
 
             #endregion
 
