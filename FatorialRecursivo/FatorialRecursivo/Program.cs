8fa7f5ed0a5072ed2a190b2977e71696acc1448a
gabriel9966 <87774582+gabriel9966@users.noreply.github.com>
2023-08-07 19:10:56 -0300

    c#

---
+++ FatorialRecursivo/FatorialRecursivo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatorialRecursivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual número vc deseja fatorar");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("O fatorial de "+num+" é "+Fatorial(num));
+            Console.ReadKey();
+        }
+
+        static int Fatorial(int num)
+        {
+            if (num > 0) 
+            { 
+                return (num * Fatorial(num - 1));
+            }
+            else
+            {
+                return 1;
+            }
+            
+
+        }
+    }
+}
